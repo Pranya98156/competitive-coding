84af5d0ab1fb063ba40d8154b6f37d952deb94da
Pranya98156 <pranya98156@gmail.com>
2026-07-18 19:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (18.06%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,14 +1,47 @@
 class Solution {
 public:
-    int numTrees(int n) {
-      //base case
+//using recursion
+int solve(int n){
+    //base case
     if(n<=1){
         return 1;
     }
     int ans=0;
     for(int i=1;i<=n;i++){
-        ans+=numTrees(i-1)*numTrees(n-i);
+        ans+=solve(i-1)*solve(n-i);
     }
     return ans;
+}
+
+//using top down approach
+int solveMem(int n,vector<int>& dp){
+    //base case
+    if(n<=1){
+        return 1;
     }
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        ans+=solveMem(i-1,dp)*solveMem(n-i,dp);
+    }
+    return dp[n]=ans;
+}
+
+//using bottom up approach
+int solveTab(int n){
+    vector<int> dp(n+1,0);
+    dp[0]=dp[1]=1;
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            dp[i]+=dp[j-1]*dp[i-j];
+        }
+    }
+    return dp[n];
+}
+    int numTrees(int n) {
+        //return solve(n);
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n,dp);
+        return solveTab(n);
+    }
+
 };
