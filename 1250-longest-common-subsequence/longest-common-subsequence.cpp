5f6cc5f119d79477c1e351aede53300d33ae65bc
Pranya98156 <pranya98156@gmail.com>
2026-07-24 13:59:56 +0530

    Time: 15 ms (92.28%) | Memory: 9.2 MB (90.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -59,10 +59,32 @@ int solveTab(string& a,string& b){
     }
     return dp[0][0];
 }
+
+
+//using space optimization
+int solveOpt(string& a,string& b){
+    vector<int> curr(b.length()+1,0);
+    vector<int> next(b.length()+1,0);
+    for(int i=a.length()-1;i>=0;i--){
+        for(int j=b.length()-1;j>=0;j--){
+            int ans=0;
+            if(a[i]==b[j]){
+                ans=1+next[j+1];
+            }
+            else{
+                ans=max(next[j],curr[j+1]);
+            }
+            curr[j]=ans;
+        }
+        next=curr;
+    }
+    return next[0];
+}
     int longestCommonSubsequence(string text1, string text2) {
        // return solve(text1,text2,0,0);
         // vector<vector<int>> dp(text1.length(),vector<int>(text2.length(),-1));
         // return solveMem(text1,text2,0,0,dp);
-        return solveTab(text1,text2);
+        //return solveTab(text1,text2);
+        return solveOpt(text1,text2);
     }
 };
