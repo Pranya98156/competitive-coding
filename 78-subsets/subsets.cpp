a02ab9b6f8956aba9966e73356d5f01d56553ece
Pranya98156 <pranya98156@gmail.com>
2026-07-28 14:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (60.41%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-void solve(vector<int>& arr,vector<vector<int>>& ans,int index,vector<int>& nums){
+void solve(vector<int>& arr, vector<vector<int>>& ans,int index,vector<int>& nums){
     //base case
     if(index==nums.size()){
         ans.push_back(arr);
         return;
     }
     arr.push_back(nums[index]);
-    //inclusion call
     solve(arr,ans,index+1,nums);
-    //exclusion call
     arr.pop_back();
     solve(arr,ans,index+1,nums);
-
 }
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<int> arr;
+       vector<int>arr;
        vector<vector<int>> ans;
        solve(arr,ans,0,nums);
        return ans;
