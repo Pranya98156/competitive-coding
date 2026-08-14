1e2f3818255c92fe7afae2a086e487b9baa0eaf0
Pranya98156 <pranya98156@gmail.com>
2026-08-14 12:37:50 +0530

    Time: 15 ms (28.89%) | Memory: 17.8 MB (55.02%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+//recursion
+int solve(int index,int operation,int k,vector<int>& prices){
+    if(index==prices.size()){
+        return 0;
+    }
+    if(operation==2*k){
+        return 0;
+    }
+    int profit=0;
+    if(operation%2==0){
+        int buykaro=-prices[index]+solve(index+1,operation+1,k,prices);
+        int skipkaro=0+solve(index+1,operation,k,prices);
+        profit=max(buykaro,skipkaro);
+    }
+    else{
+        int sellkaro=prices[index]+solve(index+1,operation+1,k,prices);
+        int ignorekaro=0+solve(index+1,operation,k,prices);
+        profit=max(sellkaro,ignorekaro);
+    }
+    return profit;
+}
+
+//memorization
+int solveMem(int index,int operation,int k,vector<int>& prices,vector<vector<int>>& dp){
+    if(index==prices.size()){
+        return 0;
+    }
+    if(operation==2*k){
+        return 0;
+    }
+    if(dp[index][operation]!=-1){
+        return dp[index][operation];
+    }
+    int profit=0;
+    if(operation%2==0){
+        int buykaro=-prices[index]+solveMem(index+1,operation+1,k,prices,dp);
+        int skipkaro=0+solveMem(index+1,operation,k,prices,dp);
+        profit=max(buykaro,skipkaro);
+    }
+    else{
+        int sellkaro=prices[index]+solveMem(index+1,operation+1,k,prices,dp);
+        int ignorekaro=0+solveMem(index+1,operation,k,prices,dp);
+        profit=max(sellkaro,ignorekaro);
+    }
+    return dp[index][operation]=profit;
+}
+    int maxProfit(int k, vector<int>& prices) {
+        //return solve(0,0,k,prices);
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int> (2*k,-1));
+        return solveMem(0,0,k,prices,dp);
+
+    }
+};
