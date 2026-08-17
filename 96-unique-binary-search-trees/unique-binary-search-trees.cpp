f8916542736900745b52a29790d10d1779702716
Pranya98156 <pranya98156@gmail.com>
2026-08-18 01:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (42.71%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -27,10 +27,25 @@ int solveMem(int n,vector<int>& dp){
     }
     return dp[n]=ans;
 }
+
+//tabulation
+int solveTab(int n){
+    vector<int> dp(n+1,0);
+    dp[0]=dp[1]=1;
+    //for number of node
+    for(int i=2;i<=n;i++){
+        // for root node
+        for(int j=1;j<=i;j++){
+            dp[i]+=dp[j-1]*dp[i-j];
+        }
+    }
+    return dp[n];
+}
     int numTrees(int n) {
         // return solve(n);
-        vector<int> dp(n+1,-1);
-        return solveMem(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n,dp);
+        return solveTab(n);
     }
 
 };
