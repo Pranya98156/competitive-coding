92224fa7a78e69c9472df12f38f7f70111932504
Pranya98156 <pranya98156@gmail.com>
2026-09-02 22:02:49 +0530

    Time: 31 ms (93.45%) | Memory: 11 MB (83.31%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -26,9 +26,32 @@ int solveMem(int start,int end,vector<vector<int>>& dp){
     return dp[start][end]=ans;
 }
 
+//tabulation
+int solveTab(int n){
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+  
+    for(int start=n;start>=1;start--){
+        for(int end=start;end<=n;end++){
+            if(start==end){
+                continue;
+            }
+            else{
+                int maxi=INT_MAX;
+                for(int i=start;i<end;i++){
+                    maxi=min(maxi,i+max(dp[start][i-1],dp[i+1][end]));
+                }
+                dp[start][end]=maxi;
+            }
+        }
+    }
+    return dp[1][n];
+
+}
+
     int getMoneyAmount(int n) {
        //return solve(1,n);
-       vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-       return solveMem(1,n,dp);
+    //    vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+    //    return solveMem(1,n,dp);
+       return solveTab(n);
     }
 };
