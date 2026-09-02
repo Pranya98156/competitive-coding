2da03d9b89a798944fa2e694f016bd2496393f8e
Pranya98156 <pranya98156@gmail.com>
2026-09-02 21:55:56 +0530

    Time: 78 ms (40.09%) | Memory: 11 MB (55.20%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-//recursion
 int solve(int start,int end){
-    //base case
     if(start>=end){
         return 0;
     }
@@ -28,30 +26,9 @@ int solveMem(int start,int end,vector<vector<int>>& dp){
     return dp[start][end]=ans;
 }
 
-//tabulation
-int solveTab(int n){
-    vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-    for(int start=n;start>=1;start--){
-        for(int end=start;end<=n;end++){
-            if(start==end){
-                continue;
-            }
-            else{
-                int ans=INT_MAX;
-                for(int i=start;i<end;i++){
-                ans=min(ans,i+max(dp[start][i-1],dp[i+1][end]));
-            }
-            dp[start][end]=ans;
-            }
-            
-        }
-    }
-    return dp[1][n];
-}
     int getMoneyAmount(int n) {
-        //return solve(1,n);
-        // vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        // return solveMem(1,n,dp);
-        return solveTab(n);
+       //return solve(1,n);
+       vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+       return solveMem(1,n,dp);
     }
 };
